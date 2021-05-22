f91a96d66027dd6ac0012d63696042c68c8da5a9
Gabriel Rufino <contato@gabrielrufino.com>
2021-05-22 10:47:23 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7f5172aa848f20dc111bf70616f48b5e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7ae4d771-4ec4-41c8-a86e-d32553b195b5'
+};
