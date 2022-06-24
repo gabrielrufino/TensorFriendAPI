fcb332d7d24f81912e2d6bc53563f07ee65637a5
Gabriel Rufino <contato@gabrielrufino.com>
2022-06-24 18:47:21 -0300

    Update strapi version

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '7f5172aa848f20dc111bf70616f48b5e'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
